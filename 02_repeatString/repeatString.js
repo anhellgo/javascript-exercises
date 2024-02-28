59d646a545011f18e743f6a9457218e84c6dfb0d
Angel <gomez.angelgomez123@gmail.com>
2024-02-28 13:49:51 -0800

    Complete Exercises 1,2,3
    Complete Exercises mentioned in title

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let newWord = '';
+    if (num < 0) { return 'ERROR'}
+    for (let i = 0; i < num; i++) {
+        newWord = newWord + word
+    }
+    return newWord;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let narray = word.split('')
+    let nword = ''
+    for (let i = (narray.length - 1); i >= 0; i--) {
+        nword += narray[i]
+    }
+    return nword
 
 };
 
